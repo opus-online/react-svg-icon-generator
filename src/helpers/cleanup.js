2ec346f09399bf71265cf1f93b42528d0b89834d
Peeter <peeter.tomberg@gmail.com>
2016-11-23 12:31:26 +0200

    Added support for className

--- src/helpers/cleanup.js
+++ src/helpers/cleanup.js
@@ -7,8 +7,8 @@ module.exports.cleanupSvg = (svg) => {
     .replace(/width="\d+"/, '')
     .replace(/height="\d+"/, '')
     .replace(/fill="#?\w+"/g, '')
-    .replace(/viewBox/, 'fill={color} height={height || size} width={width || size} onClick={onClick} style={style} viewBox')
     .replace(/\s{2,}/g, ' ')
     .replace(/class="\w+"/g, match => match.replace(/class/g, 'className'))
-    .replace(/<style>.*<\/style>/g, match => match.replace(/<style>/g, '<style>{ `').replace(/<\/style>/g, '` }</style>'));
+    .replace(/<style>.*<\/style>/g, match => match.replace(/<style>/g, '<style>{ `').replace(/<\/style>/g, '` }</style>'))
+    .replace(/viewBox/, 'fill={color} height={height || size} width={width || size} onClick={onClick} style={style} className={className} viewBox');
 };
